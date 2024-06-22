70c6525c361b6b1c3d9aea060740bb02e64393b2
aryanithahajrahmawaddah <aryanithahajrah@gmail.com>
2024-06-22 15:13:01 +0700

    React Js

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import gambar from "./assets/BTS.jpeg";
+import './App.css'
+
+function App() {
+
+  return (
+  <>
+  <div>
+  <h3>BTS</h3>
+  <p>Bangtan Sonyeondan</p>
+  <img src={gambar} alt="gambar" />
+  </div>
+  </>
+  )
+}
+
+export default App
